983adeb1612c15c073c15724ba9dcb7de3637c55
Kevin Jiang <JC@chuangs-MacBook-Air.local>
2020-04-26 22:56:05 -0400

    add all

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
+
+const navbar = document.querySelector(".navbar");
+
+const tl = new TimelineMax();
+
+tl.fromTo(navbar,1.2,{x:"-100%"},{x:"0%",ease: Power2.easeInOut});
